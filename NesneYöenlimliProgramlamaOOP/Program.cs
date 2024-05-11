b33f0cabb9aecd46894681999706f98ee5b54325
SemihErdgn <130548887+SemihErdgn@users.noreply.github.com>
2024-05-11 16:45:22 +0300

    projebaslangıc

---
+++ NesneYöenlimliProgramlamaOOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesneYöenlimliProgramlamaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tanımlama
+
+            //OOP Nesne Yönelimli Programlama 
+            //
+            //Şu ana kadar her zaman PROGRAM SINIFI içerisinde çalıştık 
+            //Şimdi 
+            //Namespaceler 
+
+            Console.WriteLine("isim =" ogr isim);
+            Console.WriteLine("Soyad = " ogr soyad);
+            Console.WriteLine()
+
+            
+               
+
+
+
+
+        }
+    }
+}
